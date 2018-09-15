9ce3ab2b5b9f3f2573a907aaa3bb5b5dd309bcc6
Michael Ackermann <michaelackermannnn@gmail.com>
2018-09-15 06:56:11 +0200

    Add build and deployment script

---
+++ build_and_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Taken from https://cloud.google.com/kubernetes-engine/docs/tutorials/hello-app
+# Assumes container cluster already exists
+
+PROJECT_ID=$1
+
+if [ -z "$1"]; then
+    echo "Usage: $0 PROJECT_ID"
+    exit -1
+fi
+
+docker build . -t gcr.io/${PROJECT_ID}/hack-zurich:latest
+gcloud docker -- push gcr.io/${PROJECT_ID}/hack-zurich:latest
+
+kubectl delete deployment/hello-web
+kubectl run hello-web --image=gcr.io/${PROJECT_ID}/hack-zurich:latest --port 5000
+
+# Assumes external port is already exposed
